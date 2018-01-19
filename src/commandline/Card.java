ff3d83a7dae99789019f53a7217ccbcbabfd6d01
Boyan Stoynov <28649002+StBoyan@users.noreply.github.com>
2018-01-19 19:06:21 +0000

    added card class methods

--- src/commandline/Card.java
+++ src/commandline/Card.java
@@ -4,5 +4,45 @@ package commandline;
  * Class to model card
  */
 public class Card {
+    String description;
+    final int NUM_OF_STATS = 5;
+    int[] stats;
 
+    /**
+     * constructor
+     * @param cardInfo line of deck.txt
+     */
+    public Card(String[] cardInfo) {
+        description = cardInfo[0];
+
+        for (int i = 0; i < NUM_OF_STATS; i++) {
+            stats[i] = Integer.parseInt(cardInfo[i]);
+        }
+    }
+
+    /**
+     * Gets card description.
+     * @return String description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Gets all card stats.
+     * @return int[] card stats
+     */
+    public int[] getStats() {
+        return stats;
+    }
+
+    /**
+     * Gets card stat at position n.
+     * @param pos stat position
+     * @return stat value
+     */
+    public int getStatAt(int pos) {
+        return stats[pos];
+    }
 }
+
