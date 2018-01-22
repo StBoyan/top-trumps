deb95d2f88df5142c4808af0db334b9eb27f380c
Ariscy <35576078+Ariscy@users.noreply.github.com>
2018-01-22 04:01:58 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,66 +1,61 @@
 package commandline;
 
 import javax.swing.JOptionPane;
+import static java.lang.Math.floorDiv;
+
+import java.util.ArrayList;
 
 public class Game {
 	
 	private Deck gamedeck = new Deck();
-	private Player human = new Player(0);
-	private Player ai1 = new Player (1);
-	private Player ai2 = new Player (2);
-	private Player ai3 = new Player (3);
-	private Player ai4 = new Player (4);
+	
 	
 	private int NumOfPlayers ;
-	private int InitialDeckSize= 40;
+	private int initialNum = 40;
 	
 	public Game() {
-		
-		
-		   
-		   
+   
 	}
 		
 		public void shareDeck() {
+			
 			gamedeck.Shuffle();
+			initialNum = 40;   
+			ArrayList<Player> playerList = new ArrayList<>();
+			ArrayList<Card> arlTemp = null;
 			
-			for (int i = 0; i <  InitialDeckSize/NumOfPlayers; i++) {
-			    
-			        Card removed = gamedeck.deck().remove(0);
-			        human.hand().add(0, removed);
-			        
-			        Card removed2 = gamedeck.deck().remove(0);
-			        ai1.hand().add(0, removed2);
-			        
-			        if (NumOfPlayers == 3) {
-			        	Card removed3 = gamedeck.deck().remove(0);
-				        ai2.hand().add(0, removed3);
-			        	
-			        }
-			        if (NumOfPlayers == 4) {
-			        	Card removed3 = gamedeck.deck().remove(0);
-				        ai2.hand().add(0, removed3);
-			        	Card removed4 = gamedeck.deck().remove(0);
-				        ai3.hand().add(0, removed4);
-			        	
-			        }
-			        if (NumOfPlayers == 5) {
-			        	Card removed3 = gamedeck.deck().remove(0);
-				        ai2.hand().add(0, removed3);
-			        	Card removed4 = gamedeck.deck().remove(0);
-				        ai3.hand().add(0, removed4);
-			        	Card removed5 = gamedeck.deck().remove(0);
-				        ai4.hand().add(0, removed5);
-			        	
-			        }
-			        
-			        
-			        
-			    
-		}
-			
-			
-	}
+	        int numOfCards = floorDiv(initialNum, NumOfPlayers);
+	        
+	        for (int i = 0; i < NumOfPlayers; i++) {
+	        	playerList.add(new Player(i));
+	        	arlTemp = new ArrayList<>();
+	        	
+	            int start = i * numOfCards;
+	            int end = (i * numOfCards + numOfCards >= initialNum) ? initialNum : 
+	             (i * numOfCards + 2 * numOfCards <= initialNum) ? i * numOfCards + numOfCards : initialNum;
+
+	          //  System.out.println("the starting point is: " + start + "\nthe end poind is: " + end);
+	            for (int j = start; j< end; j++ ) {
+	            	
+	            	arlTemp.add(gamedeck.deck().get(j));
+	            }
+	            playerList.get(i).setHand(arlTemp);
+	        }
+	        
+	        for (Player a : playerList ) {
+	        	
+	        	for (Card b : a.hand()) {
+	        		System.out.println( b.getDescription()   + "   " + b.getSize());
+	        		
+	        	}
+	        	System.out.println();
+	        }
+	       
+	        System.exit(0);
+	        
+
+				}
+
 		public static void main(String[] args) {
 			String num = JOptionPane.showInputDialog(null, "How many players (2-5)");
 			int num2 = Integer.parseInt(num);
@@ -68,37 +63,8 @@ public class Game {
 			Game x = new Game();
 			x.NumOfPlayers = num2;
 			x.shareDeck();
-		for (int i = 0; i < x.InitialDeckSize/x.NumOfPlayers; i++) {
-			
-			System.out.println(x.human.hand().get(i).getDescription() + "   " + x.human.hand().get(i).getSize());
-			
-			
-			}
-			System.out.println();
-			for (int i = 0; i < x.InitialDeckSize/x.NumOfPlayers; i++) {
-			System.out.println(x.ai1.hand().get(i).getDescription() + "   " + x.ai1.hand().get(i).getSize());
-			}
-			System.out.println();
-			if (x.NumOfPlayers == 3 || x.NumOfPlayers == 4 || x.NumOfPlayers == 5  ) {
-			for (int i = 0; i < x.InitialDeckSize/x.NumOfPlayers; i++) {
-			System.out.println(x.ai2.hand().get(i).getDescription() + "   " + x.ai2.hand().get(i).getSize());
-			}
-			}
-			System.out.println();
-			if (x.NumOfPlayers == 4 || x.NumOfPlayers == 5  ) {
-			for (int i = 0; i < x.InitialDeckSize/x.NumOfPlayers; i++) {
-			System.out.println(x.ai3.hand().get(i).getDescription() + "   " + x.ai3.hand().get(i).getSize());
-			}
-			}
-			if (x.NumOfPlayers == 5  ) {
-			System.out.println();
-			for (int i = 0; i < x.InitialDeckSize/x.NumOfPlayers; i++) {
-			System.out.println(x.ai4.hand().get(i).getDescription() + "   " + x.ai4.hand().get(i).getSize());
-			}
-			}
+		
 		}
-			
-		
-		
+	
 }
 
