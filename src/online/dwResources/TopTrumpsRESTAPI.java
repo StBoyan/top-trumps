af2662cf3fdcb1d9ef6cd52b632184844f6fc625
Boyan Stoynov <28649002+StBoyan@users.noreply.github.com>
2018-02-07 21:12:06 +0000

    had to comment out in 2 places so I can compile.
    Please check what I have commented out. It's not a permanent solution. I only did so I can compile. needs to get fixed

--- src/online/dwResources/TopTrumpsRESTAPI.java
+++ src/online/dwResources/TopTrumpsRESTAPI.java
@@ -105,9 +105,9 @@ public class TopTrumpsRESTAPI {
 
 	/** Creates a new game **/
     public Game startGame() {
-        if (game.getTopTrumpsRound().getRoundsPlayed() == 0) {
-            game.dealCards();
-        }
+//        if (game.getTopTrumpsRound().getRoundsPlayed() == 0) {            //TODO THIS IS COMMENTED OUT BECAUSE IT WON'T COMPILE. NEEDS PERMANENT SOLUTION
+//            game.dealCards();
+//        }
         return game;
     }
 
@@ -129,10 +129,10 @@ public class TopTrumpsRESTAPI {
 	 * Accordingly detects the round winner and informs if a player has been eliminated
 	 * **/
     public Game playRoundWithCategory(@QueryParam("category") int category) {
-        if (game.getPlayers()[0] == null) { // add is finished
-            throw new NotAllowedException("You lost"); //TODO something needs to happen if we loose other than notification// i.e. the game stops
-
-        }
+//        if (game.getPlayers()[0] == null) { // add is finished
+//            throw new NotAllowedException("You lost"); //TODO something needs to happen if we loose other than notification// i.e. the game stops
+//
+//        }                                                 //TODO THIS IS COMMENTED OUT BECAUSE IT WON'T COMPILE. NEEDS PERMANENT SOLUTION
 
         //TODO the game doesn't know what to do when the game finishes
         //TODO there appears to be something that's breaking midgame here
